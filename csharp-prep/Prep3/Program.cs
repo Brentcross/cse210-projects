8432def6448d72e268f3be70276c597fa2bed291
Brent Cross <crossm.brent@gmail.com>
2023-09-11 10:59:12 -0600

    added guessing game to file

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rnd = new Random();
+        int magicNumber = rnd.Next(1, 101);
+        
+        int userGuess = 0;
+        
+        Console.WriteLine("Guess the magic number anywhere between 1 and 100! ");
+
+        while (userGuess != magicNumber)
+        {
+            Console.Write("  What is your guess? ");
+            bool isNumber = int.TryParse(Console.ReadLine(), out userGuess);
+
+            if (!isNumber)
+            {
+                Console.WriteLine("  Please enter a valid number.");
+                continue;
+            }
+
+            if (userGuess < 1 || userGuess > 100)
+            {
+                Console.WriteLine("  Please enter a number between 1 and 100.");
+                continue;
+            }
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("  Higher");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("  Lower");
+            }
+        }
+
+        Console.WriteLine("  You guessed it!");
     }
 }
