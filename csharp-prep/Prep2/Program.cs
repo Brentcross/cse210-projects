0046e63a06c8e00119cf71b38a9308fe90870743
Brent Cross <crossm.brent@gmail.com>
2023-09-11 10:44:49 -0600

    changed file to grading file

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,57 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your score anywhere between 0-100: ");
+        if (double.TryParse(Console.ReadLine(), out double score))
+        {
+            if (score >= 93)
+            {
+                Console.WriteLine("Great Job! Your grade is an A.");
+            }
+            else if (score >= 90)
+            {
+                Console.WriteLine("Your grade is an A-.");
+            }
+            else if (score >= 87)
+            {
+                Console.WriteLine("Your grade is B+.");
+            }
+            else if (score >= 83)
+            {
+                Console.WriteLine("Your grade is B.");
+            }
+            else if (score >= 80)
+            {
+                Console.WriteLine("Your grade is B-.");
+            }
+            else if (score >= 77)
+            {
+                Console.WriteLine("Your grade is C+.");
+            }
+            else if (score >= 73)
+            {
+                Console.WriteLine("Your grade is C.");
+            }
+            else if (score >= 70)
+            {
+                Console.WriteLine("Your grade is C-.");
+            }
+            else if (score >= 60)
+            {
+                Console.WriteLine("Try harder, Your grade is D.");
+            }
+            else
+            {
+                Console.WriteLine("You need some work. Your grade is F.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a number between 0 and 100.");
+        }
     }
 }
